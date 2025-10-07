764e09d590a5a3af1a607b25dac46d05b5e92fff
Lauri Rooden <lauri@rooden.ee>
2025-10-07 10:04:13 +0300

    Use toXml for [Content_Types].xml

--- xlsx.js
+++ xlsx.js
@@ -7,7 +7,10 @@ var createZip = require("@litejs/zip").createZip
 	, nsRels = 'http://schemas.openxmlformats.org/officeDocument/2006/relationships/'
 	// Excel's epoch is January 1, 1900 (with a bug treating 1900 as leap year)
 	, excelEpoch = new Date(1899, 11, 30)
-	, types = ''
+	, types = [
+		{ PartName: "/styles.xml", ContentType: 'application/vnd.openxmlformats-officedocument.spreadsheetml.styles+xml' },
+		{ PartName: "/workbook.xml", ContentType: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet.main+xml' }
+	]
 	, rels = [{ Id: 'rId0', Type: nsRels + 'styles', Target: 'styles.xml' }]
 	, relsFile = (name, Relationship) => ({
 		name,
@@ -28,7 +31,7 @@ var createZip = require("@litejs/zip").createZip
 			sheet = Array.isArray(sheet)? { data: sheet } : sheet
 			i++
 			name = 'sheet' + i + '.xml'
-			types += '<Override PartName="' + name + '" ContentType="application/vnd.openxmlformats-officedocument.spreadsheetml.worksheet+xml"/>'
+			types.push({ PartName: name, ContentType: 'application/vnd.openxmlformats-officedocument.spreadsheetml.worksheet+xml' })
 			rels.push({ Id: 'rId' + i, Type: nsRels + 'worksheet', Target: name })
 			sheets += '<sheet name="' + (sheet.name || 'Sheet' + i) + '" sheetId="' + i + '" r:id="rId' + i + '"/>'
 			var cols = sheet.cols
@@ -65,11 +68,15 @@ var createZip = require("@litejs/zip").createZip
 	files.unshift(
 		{
 			name: '[Content_Types].xml',
-			content: xmlHead + '<Types xmlns="http://schemas.openxmlformats.org/package/2006/content-types"><Default Extension="rels" ContentType="application/vnd.openxmlformats-package.relationships+xml"/><Default Extension="xml" ContentType="application/xml"/><Override PartName="/styles.xml" ContentType="application/vnd.openxmlformats-officedocument.spreadsheetml.styles+xml"/><Override PartName="/workbook.xml" ContentType="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet.main+xml"/>' + types + '</Types>'
+			content: xmlHead + toXml('Types', { xmlns: 'http://schemas.openxmlformats.org/package/2006/content-types' }, {
+				Default: [
+					{ Extension: "rels", ContentType: "application/vnd.openxmlformats-package.relationships+xml" },
+					{ Extension: "xml", ContentType: "application/xml" }
+				],
+				Override: types
+			})
 		},
-		relsFile('_rels/.rels', [
-			{ Id: 'rId1', Type: nsRels + 'officeDocument', Target: 'workbook.xml' }
-		]),
+		relsFile('_rels/.rels', [{ Id: 'rId1', Type: nsRels + 'officeDocument', Target: 'workbook.xml' }]),
 		relsFile('_rels/workbook.xml.rels', rels),
 		{
 			name: 'styles.xml',
